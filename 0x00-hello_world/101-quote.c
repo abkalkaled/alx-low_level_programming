d736b437ad655e212d357bc9ce87b22fbf89ccbb
khaled <aminukhaleedabiola@gmail.com>
2023-09-07 15:41:25 -0700

    using write to display

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
@@ -6,9 +7,6 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful" - Dora Korpar, 201 5- 10 \
-			  - 19";
-	size_t len = sizeof (message) - 1;
-	write(STDOUT_FILENO, message, len);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
