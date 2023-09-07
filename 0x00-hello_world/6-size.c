a6c9ab48bde574bed9f3afef711dd209cb5c74f5
khaled <aminukhaleedabiola@gmail.com>
2023-09-07 14:01:21 -0700

    print sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: return a value of 0
+ */
+int main(0)
+{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	printf(sizeof(float));
+	return (0);
+}
