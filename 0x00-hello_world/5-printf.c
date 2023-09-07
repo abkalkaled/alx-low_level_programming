f2f2160f032f00d1a71654b2710bc741b088dee9
khaled <aminukhaleedabiola@gmail.com>
2023-09-07 13:25:59 -0700

    first printf program compiled

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 or 1
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcomr is a piece of art,\n");
+	return (0);
+}
