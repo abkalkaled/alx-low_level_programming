485a16760cf79709e581aa30082f93a6d6c9a5e1
khaled <aminukhaleedabiola@gmail.com>
2023-09-08 09:52:15 -0700

    2-print a - z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ *
+ * Return: gives a value of 0
+ */
+int main(void)
+{
+	char n = 97; /*97 is a in ASCII*/
+
+	while (n <= 122, n++)
+	{
+		putchar('n');
+	}
+
+		return (0);
+}
