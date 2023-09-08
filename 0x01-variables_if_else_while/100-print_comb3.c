3044fe90c40aeeb9879b3fe424b0d8c79df8e65e
khaled <aminukhaleedabiola@gmail.com>
2023-09-08 16:09:06 -0700

    2 digits combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int unit = 0;
+	int tens = 0;
+
+	for (unit = 0; unit <= 9; unit++)
+	{
+		for (tens = unit + 1; tens <= 9; tens++)
+	{
+		putchar((tens % 10) + '0');
+		putchar((unit % 10) + '0');
+		if (unit == 8 && tens == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
