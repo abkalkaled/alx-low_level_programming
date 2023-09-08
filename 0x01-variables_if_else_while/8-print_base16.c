d0b979f49e0cc061debf8f5bd531b6a6edcc5776
khaled <aminukhaleedabiola@gmail.com>
2023-09-08 14:17:36 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - code entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (b = 65; b <= 70; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
