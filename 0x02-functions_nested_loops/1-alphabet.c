5580b549edfacf66c07184f1cadba9e572283a5d
khaled <aminukhaleedabiola@gmail.com>
2023-09-12 07:28:59 -0700

    lowercase alphabets printed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - starts code running
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for(alphabet = 97; alphabet <= 122; alphabet++);
+	_putchar('alphabet');
+	_putchar('\n');
+	return (0);
+
+}
