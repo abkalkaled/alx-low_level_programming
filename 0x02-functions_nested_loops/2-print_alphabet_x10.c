1eb489d0a4c557a57b702b86422dc4b1a53d5e25
khaled <aminukhaleedabiola@gmail.com>
2023-09-12 09:46:14 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	i = 0;
 
+	i = 0;
 	while (i < 10)
 	{
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar('ch');
+		_putchar(ch);
 	}
 	_putchar('\n');
 	i++;
