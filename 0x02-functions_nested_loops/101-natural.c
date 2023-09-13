ad9b45d173ed67ded7326407145da49118416049
khaled <aminukhaleedabiola@gmail.com>
2023-09-13 13:08:59 -0700

    updated

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	int sum = 0;
+	int n, sum = 0;
 
-	for (int n = 1; n < 1024; n++)
+	while (n < 1024)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
 			sum += n;
 		}
+		n++;
 	}
 	printf("sum of mult 3 and 5 below 1024 is: %d\n", sum);
 	return (0);
