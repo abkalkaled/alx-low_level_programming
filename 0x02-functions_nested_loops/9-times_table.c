76fe07bd99b1e98cbcf5bd14f767fdb276fe1cbb
khaled <aminukhaleedabiola@gmail.com>
2023-09-13 01:17:37 -0700

    table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,18 +6,32 @@
  */
 void times_table(void)
 {
-	int a, b;
-	int c = a * b;
+	int a, b, c, d, e;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-
-			_putchar(44);
-			_putchar(32);
-			_putchar('c');
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(e);
+				_putchar(d);
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c);
+			}
 		}
 		_putchar('\n');
 	}
