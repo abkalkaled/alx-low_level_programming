2ec9b691ad937b2b6f34552e92cc332cc46d70d3
khaled <aminukhaleedabiola@gmail.com>
2023-09-14 13:57:47 -0700

    betty checked isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isdigit _ checks for digit starts
+ * _isdigit - checks for digit starts
  * @c: the digit in check
  * Return: 1 if true, 0 false
  */
 int _isdigit(int c)
 {
-	if (c >=48 && c<= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
