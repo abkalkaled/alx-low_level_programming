132e8edfd31feb0d320810f16f0d461846d965f8
khaled <aminukhaleedabiola@gmail.com>
2023-09-15 10:25:49 -0700

    lines printed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - starts to print line
+ * @n: number of times to print _
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (n = 2; n <= 10; n++)
+	{
+		_putchar(95);
+		_putchar('\n');
+	}
+	}
+}
