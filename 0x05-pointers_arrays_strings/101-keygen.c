e085f982e455a39fa3e9db7d4db1c06925134e31
khaled <aminukhaleedabiola@gmail.com>
2023-09-19 13:56:13 -0700

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-// Function to generate a random character
-char getRandomChar() {
-    const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+/**
+ * getRandomChar - gets random password
+ *
+ * Return: always 0;
+ */
+char getRandomChar(void)
+{
+	const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
     int index = rand() % (sizeof(charset) - 1);
-    return charset[index];
-}
 
-// Function to generate a random password
-void generatePassword(char *password, int length) {
-    for (int i = 0; i < length; i++) {
+    return (charset[index]);
+}
+void generatePassword(char *password, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
         password[i] = getRandomChar();
     }
     password[length] = '\0';
 }
 
-int main() {
-    srand(time(NULL)); // Seed the random number generator
+int main(void)
+{
+    srand(time(NULL));
 
-    int length = 12; // Change the length as needed
-    char password[length + 1]; // +1 for the null terminator
+    int length = 12;
+    char password[length + 1];
 
     generatePassword(password, length);
-
     printf("Generated Password: %s\n", password);
-
-    return 0;
+    return (0);
 }
 
