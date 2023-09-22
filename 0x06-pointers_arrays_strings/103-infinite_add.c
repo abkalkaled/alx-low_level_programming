1fcbdcb84506accc9448d00d6afdfcbd1268e16f
khaled <aminukhaleedabiola@gmail.com>
2023-09-22 11:44:37 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,11 +11,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a, b, c, d, e, f;
 	
-	for (a = 0; n1[a]; a++);
-		for (b = 0; n2[b]; b++);
+	for (a = 0; n1[a]; a++)
+	{
+		for (b = 0; n2[b]; b++)
+		{
 			if (a > size_r || b > size_r)
 				return (0);
-				e = 0;
+		}
+		e = 0;
+	}
 
 			for (a -= 1, b -= 1, c = 0; c < size_r - 1; a--, b--, c++)
 			{
