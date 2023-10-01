96e24fcb6051cd6a91b47d6856de48fe4b91d41b
khaled <aminukhaleedabiola@gmail.com>
2023-10-01 10:16:32 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
-* find_root - find square root of n, starting from the smallest possible, 0
-* @n: n
-* @root: test this root
+* seek_root - find square root of n, starting from the smallest possible, 0
+* _sqrt_recursion - recursion sqrt starts here
+* @n: number with natural square root
+* @root: finds root
 * Return: natural square root, or -1 if not natural root
 */
 
-int find_root(int n, int root)
+int seek_root(int n, int root)
 {
 	if (root * root > n)
 		return (-1);
 	if (root * root == n)
 		return (root);
-	return (find_root(n, root + 1));
+	return (seek_root(n, root + 1));
 }
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (find_root(n, 0));
+	return (seek_root(n, 0));
 }
