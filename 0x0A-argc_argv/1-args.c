0386d702977dfed700e4681f4f533a30c17fa4a4
khaled <aminukhaleedabiola@gmail.com>
2023-10-02 13:18:47 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - start point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0;
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
