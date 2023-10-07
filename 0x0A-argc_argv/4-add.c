38d9e9716ec55bae4655dd64158786cd5645ba97
khaled <aminukhaleedabiola@gmail.com>
2023-10-07 06:42:13 -0700

    function to add positive

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,24 +3,25 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- * main - funxtion to add
+ * main - function to add
  * @argc: argument count in int
  * @argv: argument vector in str
- * Return 0 or 1
+ * Return: 0 or 1
  */
 int main(int argc, char *argv[])
 {
-	int i, j, num, sum = 0;
-	
+	int i, j, sum = 0;
+
 	if (argc == 1)
-		{
-			printf("0\n");
-			return (0);
-		}
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		char *arg = argv[i];
-		for(j = 0; arg[i] != '\0'; j++)
+
+		for (j = 0; arg[i] != '\0'; j++)
 		{
 			if (!isdigit(arg[j]))
 			{
@@ -28,8 +29,10 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		num = atoi(arg);
-		sum += num;
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
