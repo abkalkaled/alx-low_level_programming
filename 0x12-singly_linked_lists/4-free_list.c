027cf7e23698914810c703e60d2df1054959e19a
abkalkaled <aminukhaleedabiola@gmail.com>
2023-10-19 14:36:38 -0700

    donr

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - free list
+ * *head: points to head of list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head -> next;
+		free(head -> str);
+		free(head);
+		head = temp;
+	}
+}
