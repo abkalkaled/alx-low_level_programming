d7a454fa543f116f5d6cff261b8cfe692bca51dc
abkalkaled <aminukhaleedabiola@gmail.com>
2023-10-24 02:37:59 -0700

    free trial

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - function to free list data
+ * @head: pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = *head->next;
+		free(*head);
+		temp = *head;
+	}
+	*head = NULL;
+}
