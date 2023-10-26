f2b0984c8cb6e1c2841b5b27b9e053f7024c64a5
abkalkaled <aminukhaleedabiola@gmail.com>
2023-10-26 14:23:49 -0700

    0000 fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	
+
 	if (b == NULL || *b == '\0')
-        return (0);
+		return (0);
 	while (*b)
-    {
-	    if (*b == '0' || *b == '\0')
-	    {
-		    num = (num << 1) + (*b - '0');
-	    }
-	    else
-	    {
-		    return (0);
-	    }
-	    b++;
-    }
+	{
+		if (*b == '0' || *b == '\0')
+		{
+			num = (num << 1) + (*b - '0');
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
 	return (num);
 }
