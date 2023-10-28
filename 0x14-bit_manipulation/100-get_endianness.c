0a8cb41b2fe3acccbf745abe5f0a0f94f188df06
abkalkaled <aminukhaleedabiola@gmail.com>
2023-10-28 11:02:43 -0700

    betty fixed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
  * get_endianness - returns the value of a bit at a given index
- * @num: number to get the bit from
- * @p: the index, starting from 0 of the bit desired
  * Return: 1 or 0
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	unsigned char *p = (unsigned char*)&num;
+	unsigned char *p = (unsigned char *)&num;
 
 	if (*p == 1)
 		return (1);
