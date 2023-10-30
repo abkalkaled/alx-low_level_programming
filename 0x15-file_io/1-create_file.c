f09db249a9a3dd7113e5c64c3d62ec8e53c492fd
abkalkaled <aminukhaleedabiola@gmail.com>
2023-10-30 13:57:09 -0700

    file created

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w_check, i;
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1; // Invalid filename
-    }
-
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        return -1; // File could not be created or opened
-    }
-
-    if (text_content != NULL) {
-        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1) {
-            close(fd);
-            return -1; // File could not be written
-        }
-    }
-
-    close(fd);
-    return 1; // File created and written successfully
-
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (i = 0; *(text_content + i); i++);
+		w_check = write(fd, text_content, i);
+		if (w_check == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
