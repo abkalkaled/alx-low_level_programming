a51654488e2efcfff0b6173cda13898d39706f64
abkalkaled <aminukhaleedabiola@gmail.com>
2023-12-24 12:59:32 -0800

    syntax error fixed

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size;
+	index = key_index((const unsigned char *)key, ht->size);
 			current = ht->array[index];
 			while (current != NULL)
 			{
@@ -23,4 +23,3 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 			}
 			return (NULL);
 }
-
